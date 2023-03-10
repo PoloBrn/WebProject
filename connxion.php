532b4f081428e4ae1852a5e1a62aed5f1b1321fe
Layauu <80603850+Layauu@users.noreply.github.com>
2023-03-10 12:12:15 +0100

    Update connxion.php

--- connxion.php
+++ connxion.php
@@ -1,7 +1,9 @@
 <?php 
-$dsn = 'mysql:host=34.155.93.202;dbname=web-project-v0';
+//$dsn = 'mysql:host=34.155.93.202;dbname=web-project-v0'; // Serveur Google Cloud
+$dsn = 'mysql:host=127.0.0.1;dbname=webproject_v0'; // Serveur Localhost
 
-$pdo = new PDO($dsn, 'root' , 'cesi'); 
+//$pdo = new PDO($dsn, 'root' , 'cesi'); // Serveur Google Cloud
+$pdo = new PDO($dsn, 'root' , ''); // Serveur Localhost
 
 // $Eleve_Result= mysql_query ( SELECT * FROM users ) ;
 $stmt = $pdo->query('select * from users');
