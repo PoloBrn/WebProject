3484a3208c1b09f9dece7e035e1576a3b6108e87
Layauu <80603850+Layauu@users.noreply.github.com>
2023-03-20 12:24:09 +0100

    Create navbar.php

---
+++ includes/navbar.php
@@ -0,0 +1,68 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">
+            <!--<img src="assets/images/logo_ligne.png" alt="Logo" height="50"  class="d-inline-block align-text-middle">-->
+            INTERNCHIPS
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <?php
+
+
+                switch ($_SESSION['id_role']) {
+                    case 1: ?>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Gestion des offres</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="companies.php">Gestion des entreprises</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="users.php">Gestion des utilisateurs</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="campus.php">Gestion des campus</a>
+                        </li>
+                    <?php break;
+
+                    case 2: ?>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Gestion des offres</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="companies.php">Gestion des entreprises</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="users.php">Gestion des étudiants</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">A propos</a>
+                        </li>
+                    <?php break;
+
+                    case 3: ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Les offres</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="companies.php">Les entreprises</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Mon profil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">A propos</a>
+                        </li>
+                <?php break;
+                }
+                ?>
+                <li class="nav-item">
+                    <a class="nav-link active" href="../controller/logoutAction.php">Se déconnecter</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
