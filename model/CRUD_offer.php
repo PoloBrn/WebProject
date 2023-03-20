8fac5e5dc2d361086a9dd0b05ef7754df53fd3c4
PoloBrn <115800230+PoloBrn@users.noreply.github.com>
2023-03-20 11:58:28 +0100

    Offer

---
+++ model/CRUD_offer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MODELE;
+
+require_once 'database.php';
+
+class CRUD_offer extends Database
+{
+
+    function create($array)
+    {
+    }
+    function update($array)
+    {
+    }
+    function delete($array)
+    {
+    }
+    function get($array)
+    {
+    }
+}
